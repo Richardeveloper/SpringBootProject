7b8f658ffca008792b76e360167b8a863d200497
Richard <Richard_ar@hotmail.com>
2020-07-01 09:03:57 -0300

    Service Layer, Component Registration

--- src/main/java/com/richardeveloper/resources/UserResource.java
+++ src/main/java/com/richardeveloper/resources/UserResource.java
@@ -1,11 +1,39 @@
 package com.richardeveloper.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.richardeveloper.entities.User;
+import com.richardeveloper.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		if(users.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
 }
